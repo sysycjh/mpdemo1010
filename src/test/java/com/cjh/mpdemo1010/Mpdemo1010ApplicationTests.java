0b4391b7d2c78891fa665efac7e0289926b73622
chenxi1006968489 <1006968489@qq.com>
2022-10-12 10:04:49 +0800

    commit1

--- src/test/java/com/cjh/mpdemo1010/Mpdemo1010ApplicationTests.java
+++ src/test/java/com/cjh/mpdemo1010/Mpdemo1010ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.cjh.mpdemo1010;
 
+import com.cjh.mpdemo1010.entity.User;
+import com.cjh.mpdemo1010.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Mpdemo1010ApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     void contextLoads() {
+        User user = userMapper.selectById(1);
+        System.out.println(user);
     }
 
 }
